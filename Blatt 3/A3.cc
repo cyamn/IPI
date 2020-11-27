737fc700249bd1a501e2a2cf080484a2105c64bc
Ingmar <ingmar@lowack.com>
2020-11-27 10:17:10 +0100

    final version

--- Blatt 3/A3.cc
+++ Blatt 3/A3.cc
@@ -1,4 +1,7 @@
-#include "quadrat.cc"
+#include "fcpp.hh"
+
+// Gruppe: Lowack, Meier, Jaschke
+
 
 // a)
 bool ungerade(int zahl)
@@ -12,12 +15,21 @@ int multsignal(int a, int b) {
   return a*b;
 }
 
+int quadrat(int x)
+{
+  return multsignal(x,x);
+}
+
 // b)
-// c) (exponent,#multiplikationen): (3,3), (5,4), (6,4)
+// c) (exponent, Anzahl multiplikationen):
+//    Bei schneller Rekursion: (8,4), (11,6), (15,7) 
+//    Bei normaler Rekursion immer n-1: (8,7), (11,10), (15,14)
 // d) Abarbeitung entspricht dem Substitutionsmodell, denn ansonsten hätten wir mehr
-//    Multiplikationen als in c) angegeben. Denn dann dann würde quadrat(exp(b,e))
-//    compiliert zu exp(b,e)*exp(b,e) und exp(b,e) müsste 2 mal berechnet werden.
-//    Es wird aber compiliert zu e=exp(b,e); e*e und exp muss nur einmal berechnet werden.
+//    Multiplikationen als in c) angegeben. Denn dann dann würde quadrat(exp(b,e/2))
+//    compiliert zu exp(b,e/2)*exp(b,e/2) und exp(b,e/2) müsste 2 mal berechnet werden.
+//    Es wird aber compiliert zu e=exp(b,e/2); e*e und exp muss nur einmal berechnet werden.
+//    Die schnelle exponentiation nicht substitutive ausgewertet würde genauso viele
+//    Multiplikationen benötigen wie die normale Rekursion.
 int exponentation(int basis, int exponent)
 {
   return cond(exponent<=0, cond(exponent==0,1,0),
@@ -27,13 +39,13 @@ int exponentation(int basis, int exponent)
 int main ()
 { 
   int a;
-  a=exponentation(2,3);
+  a=exponentation(2,8);
   print(a);
   print("------");
-  a=exponentation(2,5);
+  a=exponentation(2,11);
   print(a);
   print("------");
-  a=exponentation(2,6);
+  a=exponentation(2,15);
   print(a);
   print("------");
   return 0;
